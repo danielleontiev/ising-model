95684dc6d53d558029aca6938fa21ca847c80a85
Leontev Daniil <prohormitrich53@gmail.com>
2018-04-18 07:17:05 +0300

    Correct energy calculation

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import random
 import math
+from itertools import cycle
 import matplotlib.pyplot as plot
 
 
 class IsingModel:
 
-    def __init__(self, linear_size, exchange_interaction, temperature) -> None:
+    def __init__(self, linear_size, sum_threshold, exchange_interaction, temperature) -> None:
         self.linear_size = linear_size
-        self.step = 0
         self.temperature = temperature
         self.exchange_interaction = exchange_interaction
         self.spins = [[random.choice((1, -1)) for _ in range(linear_size)] for _ in range(linear_size)]
+        self.sum_energy = 0
+        self.step = 0
+        self.sum_threshold = sum_threshold
+        self.lattice_iterator = cycle(((i, j for i in range(linear_size)) for j in range(linear_size)))
         self.figure = None
         self.matrix = None
         self.plot_initialized = False
@@ -22,21 +26,15 @@ class IsingModel:
         right = j + 1 if j < self.linear_size - 1 else 0
         return self.spins[i][left] + self.spins[i][right] + self.spins[up][j] + self.spins[down][j]
 
-    # @property
-    # def full_energy(self):
-    #     full_energy = 0
-    #     for i in range(self.linear_size):
-    #         for j in range(self.linear_size):
-    #             full_energy += -self.exchange_interaction * self.spins[i][j] * self._neighbour_spins_sum(i, j)
-    #     return full_energy / 4
-
     def mc_step(self):
         self.step += 1
-        i = random.randrange(self.linear_size)
-        j = random.randrange(self.linear_size)
-        d_e = 2 * self.exchange_interaction * self.spins[i][j] * self._neighbour_spins_sum(i, j)
-        if d_e <= 0 or random.random() <= math.exp(-d_e / self.temperature):
-            self.spins[i][j] *= -1
+        for i in range(self.linear_size):
+            for j in range(self.linear_size):
+                d_e = 2 * self.exchange_interaction * self.spins[i][j] * self._neighbour_spins_sum(i, j)
+                if d_e <= 0 or random.random() <= math.exp(-d_e / self.temperature):
+                    self.spins[i][j] *= -1
+                    if self.step > self.sum_threshold:
+                        self.sum_energy += d_e
 
     def _initialize_plot(self):
         plot.ion()
@@ -53,8 +51,11 @@ class IsingModel:
             self.figure.canvas.draw()
 
 
-im = IsingModel(100, 1, 0.1)
+im = IsingModel(100, 50, 1, 0.1)
 n = 10 ** 5
-for k in range(100000):
-    im.mc_step()
-    im.plot_state(1000)
+for t in range(4, 18, 1):
+    tt = t / 4
+    im.temperature = tt
+    for k in range(100):
+        im.mc_step()
+    print(tt, im.sum_energy)
